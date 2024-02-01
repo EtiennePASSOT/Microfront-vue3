f3c362cc4814996eb95a8e91b5ce2ed8e9a0a7fd
Etienne PASSOT <etienne.passot@steeple.com>
2024-02-01 18:40:34 +0100

    refactor and remove useless files

--- src/App.vue
+++ src/App.vue
@@ -1,96 +1,47 @@
 <script setup>
-import { RouterLink, RouterView } from "vue-router";
-import HelloWorld from "./components/HelloWorld.vue";
 import { onMounted } from "vue";
 
-const loadApp = async () => {
-  const manifest = await fetch(`/dist/my-micro-app/.vite/manifest.json`);
-  const jsonManifest = await manifest.json();
+const BASE_URL_MICRO_APP = "/dist/my-micro-app";
 
-  console.log(jsonManifest);
-  const script = document.createElement("script");
-  script.src = `/dist/my-micro-app/${jsonManifest["index.html"].file}`;
-  script.type = "module";
-  document.head.appendChild(script);
-
-  setTimeout(() => {
-    window.apps.microApp(document.getElementById("my-micro-app"));
-  }, 1000);
+const loadManifest = async () => {
+  const manifest = await fetch(`${BASE_URL_MICRO_APP}/.vite/manifest.json`);
+  return await manifest.json();
 };
 
-onMounted(() => {
-  loadApp();
+const loadScript = async (manifest) => {
+  const script = document.createElement("script");
+  script.src = `${BASE_URL_MICRO_APP}/${manifest["index.html"].file}`;
+  script.type = "module";
+  document.head.appendChild(script);
+};
+
+function loadApp() {
+  const intervalId = setInterval(() => {
+    if (window.apps && window.apps.microApp) {
+      window.apps.microApp(document.getElementById("my-micro-app"));
+      clearInterval(intervalId);
+    }
+  }, 10);
+}
+
+onMounted(async () => {
+  const manifest = await loadManifest();
+  loadScript(manifest);
+  loadApp(manifest);
 });
 </script>
 
 <template>
-  <div>
-    My app
+  <div class="my-app">
+    <h1>My app</h1>
 
     <div id="my-micro-app"></div>
   </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+<style lang="css">
+.my-app {
+  padding: 16px;
+  background-color: #f0f0f0;
 }
 </style>
--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { fileURLToPath, URL } from "node:url";
 
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import { copy } from "vite-plugin-copy";
 import { viteStaticCopy } from "vite-plugin-static-copy";
 
 // https://vitejs.dev/config/
